93ebf6baa521304197670b754e65170f3bde3ae3
Laken Harville <rlakenharville@gmail.com>
2022-07-17 00:46:38 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace OpenWeatherMapAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            Console.WriteLine("Enter the API key: ");
+            var api_Key = Console.ReadLine();
+
+            while (true)
+            {
+                Console.WriteLine();
+
+                Console.WriteLine("Enter city name: ");
+
+                var city_name = Console.ReadLine();
+
+                var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?q={city_name}&units=imperial&appid={api_Key}";
+
+                var response = client.GetStringAsync(weatherURL).Result;
+
+                var formattedResponse = JObject.Parse(response).GetValue("main").ToString();
+
+                Console.WriteLine(formattedResponse);
+
+                Console.WriteLine();
+
+                Console.WriteLine("Would you like to enter a different city?");
+
+                var userInput = Console.ReadLine();
+
+                Console.WriteLine();
+
+                if (userInput.ToLower() == "no")
+                {
+                    break;
+                }
+
+            }
+
+        }
+    }
+}
